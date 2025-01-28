2d8574b99bd930bb959e91f91eb86900a5687d9c
julianaMonteiro <juliana47monteiro@gmail.com>
2025-01-28 11:15:32 -0300

    configuração e model

---
+++ src/main/java/com/generation/blogpessoal/model/Produto.java
@@ -0,0 +1,69 @@
+package com.generation.blogpessoal.model;
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é Obrigatório!")
+	private String nome;
+	
+	@NotBlank(message = "O atributo descrição é Obrigatório!")
+	private String descricao;
+	
+	@DecimalMin(value = "0.0", inclusive = false, message = "O valor deve ser maior que zero.")
+	@Column(nullable = false, precision = 10, scale = 2)
+	private BigDecimal valor;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("categoria")
+	private Categoria categotia;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+}
